7ed49a1b34e3556a5e21b1f057afadd840b89188
Marcel Belato <41217264+marcelbelato@users.noreply.github.com>
2021-01-24 13:29:36 -0300

    Add files via upload

---
+++ AuthenticationWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthenticationWebAPI.Services;
+using AuthenticationWebAPI.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using AuthenticationWebAPI.Models;
+
+namespace AuthenticationWebAPI.Controllers
+{
+    [Route("v1/account")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<dynamic>> Authenticate([FromBody] User model)
+        {
+            var user = UserRepository.Get(model.Username, model.Password);
+
+            if (user == null)
+            {
+                return NotFound(new { message = "Usuário ou senha inválidos!" });
+            }
+
+            var token = TokenService.GenerateToken(user);
+            user.Password = "";
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+
+        [HttpGet("anonymous")]
+        [AllowAnonymous]
+        public string Anonymous() => "Anônimo";
+
+        [HttpGet("authenticated")]
+        [Authorize]
+        public string Authenticated() => $"Autenticado - {User.Identity.Name}";
+
+        [HttpGet("employee")]
+        [Authorize(Roles = "employee, manager")]
+        public string Employee() => "Funcionário";
+
+        [HttpGet("manager")]
+        [Authorize(Roles = "manager")]
+        public string Manager() => "Gerente";
+    }
+}
